05b86e3f1f92b6df9d86f5fff5ca62e0623b0dd3
Emerson <hi@emersonthis.com>
2020-10-03 10:56:19 -0700

    first swipe

---
+++ convert.js
@@ -0,0 +1,42 @@
+const parseString = require('xml2js').parseString;
+const TurndownService = require('turndown');
+const turndownService = new TurndownService();
+const fs = require('fs');
+
+const exportPath = process.argv[2];
+
+const fileContents = fs.readFileSync(exportPath, 'utf8');
+
+let json = null;
+
+parseString(fileContents, function (err, result) {
+    json = result
+});
+
+const posts = json.rss.channel[0].item;
+
+for (let i=0; i<posts.length; i++) {
+	const post = posts[i];
+
+	let fileString = `---\ntitle: ${post.title}\n---\n\n`;
+
+	const htmlContent = post['content:encoded'][0];
+	fileString += turndownService.turndown(htmlContent);
+
+	const postDate = post['wp:post_date'][0].split(' ')[0];
+	const fileName = `${postDate}-${post['wp:post_name']}`;
+
+	fs.writeFile(
+		'./output/'+fileName+'.md', 
+		fileString, 
+		'utf8', 
+		(err) => {
+			if (err) {
+				console.error(err);
+			}
+		}
+	);
+
+}
+
+console.log('Done!');
